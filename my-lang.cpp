ffc904a561f4018a308f8cbcc7431165f1eec0c1
Rafael Herrera <randres2011@gmail.com>
2022-01-06 15:12:31 -0500

    Abstract syntax tree

--- my-lang.cpp
+++ my-lang.cpp
@@ -1,14 +1,15 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
 //===----------------------------------------------------------------------===//
 /*  LEXER
     It breaks the input up into “tokens”. Each token returned by the lexer includes 
     a token code and potentially some metadata (e.g. the numeric value of a number)
+    The lexer returns tokens [0-255] if it is an unknown character, otherwise one
+    of these for known things.
 */
 //===----------------------------------------------------------------------===//
-
-#include <string>
-#include <iostream>
-// The lexer returns tokens [0-255] if it is an unknown character, otherwise one
-// of these for known things.
 enum Token {
     tok_eof = -1,
     // commands
@@ -16,7 +17,6 @@ enum Token {
     // primary
     tok_identifier = -4, tok_number = -5,
 };
-
 /*
     Each token returned by our lexer will either be one of the Token enum values or it will be 
     an ‘unknown’ character like ‘+’, which is returned as its ASCII value. If the current token 
@@ -29,8 +29,7 @@ enum Token {
 static std::string IdentifierStr;  // Filled in if tok_identifier
 static double NumVal;              // Filled in if tok_number
 
-
-/// gettok - Return the next token from standard input.
+// gettok - Return the next token from standard input.
 static int gettok() {
     static int LastChar = ' ';
 
@@ -38,7 +37,7 @@ static int gettok() {
     while (isspace(LastChar))
         LastChar = getchar();
     // Recognize identifiers and specific keywords
-    if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+    if (isalpha(LastChar)) { // Identifier: [a-zA-Z][a-zA-Z0-9]*
         IdentifierStr = LastChar;
         while (isalnum((LastChar = getchar())))
             IdentifierStr += LastChar;
@@ -77,6 +76,70 @@ static int gettok() {
     LastChar = getchar();
     return ThisChar;
 }
+
+//===----------------------------------------------------------------------===//
+/*  ABSTRACT SYNTAX TREE
+    The AST for a program captures its behavior in such a way that it is easy for later 
+    stages of the compiler (e.g. code generation) to interpret. 
+*/
+//===----------------------------------------------------------------------===//
+// ExprAST - Base class for all expression nodes.
+class ExprAST {
+public:
+    virtual ~ExprAST() {}
+};
+
+// NumberExprAST - Expression class for numeric literals like "1.0".
+class NumberExprAST : public ExprAST {
+    double Val;
+public:
+    NumberExprAST(double val) : Val(val) {}
+};
+
+// VariableExprAST - Expression class for referencing a variable, like "a".
+class VariableExprAST : public ExprAST {
+    std::string Name;
+public:
+    VariableExprAST(const std::string &name) : Name(name) {}
+};
+
+// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+    char Op; // + - * / > <
+    ExprAST *LHS, *RHS;
+public:
+    BinaryExprAST(char op, ExprAST *lhs, ExprAST *rhs)
+        : Op(op), LHS(lhs), RHS(rhs) {}
+};
+
+// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+    std::string Callee;
+    std::vector<ExprAST*> Args;
+public:
+    CallExprAST(const std::string &callee, std::vector<ExprAST*> &args)
+        : Callee(callee), Args(args) {}
+};
+
+// PrototypeAST - This class represents the "prototype" for a function,
+// which captures its name, and its argument names (thus implicitly the number
+// of arguments the function takes).
+class PrototypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+public:
+    PrototypeAST(const std::string &name, const std::vector<std::string> &args)
+        : Name(name), Args(args) {}
+};
+
+// FunctionAST - This class represents a function definition itself.
+class FunctionAST {
+    PrototypeAST *Proto;
+    ExprAST *Body;
+    public:
+        FunctionAST(PrototypeAST *proto, ExprAST *body)
+            : Proto(proto), Body(body) {}
+};
 
 // int main () {
 //     while (true) {
