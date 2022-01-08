ceaeebcf5d960c24a9f61dd89b84aef1ae10e2de
Rafael Herrera <randres2011@gmail.com>
2022-01-07 20:40:55 -0500

    LLVM IR

--- my-lang.cpp
+++ my-lang.cpp
@@ -131,8 +131,8 @@ class CallExprAST : public ExprAST {
     std::string Callee;
     std::vector<std::unique_ptr<ExprAST>> Args;
 public:
-    CallExprAST(const std::string &callee, std::vector<std::unique_ptr<ExprAST>> &args)
-        : Callee(callee), Args(std::move(Args)) {}
+    CallExprAST(const std::string &callee, std::vector<std::unique_ptr<ExprAST>> args)
+        : Callee(callee), Args(std::move(args)) {}
     Value *codegen() override;
 };
 
@@ -352,11 +352,140 @@ static std::unique_ptr<PrototypeAST> ParseExtern() {
 }
 
 //===----------------------------------------------------------------------===//
-// Top-Level parsing
+// CODE GENERATION
+// Transform the Abstract Syntax Tree into LLVM IR.
+//===----------------------------------------------------------------------===//_
+
+// Owns a lot of core LLVM data structures, such as the type and constant value tables.
+static std::unique_ptr<LLVMContext> TheContext;
+// Contains functions and global variables.
+static std::unique_ptr<Module> TheModule;
+// Helper object that makes it easy to generate LLVM instructions
+static std::unique_ptr<IRBuilder<>> Builder;
+// Keeps track of which values are defined in the current scope and what 
+// their LLVM representation is
+static std::map<std::string, Value *> NamedValues;
+
+Value *LogErrorV(const char *Str) {
+    LogError(Str);
+    return nullptr;
+}
+
+// numeric constants
+Value *NumberExprAST::codegen() {
+    return ConstantFP::get(*TheContext, APFloat(Val));
+}
+
+// variables
+Value *VariableExprAST::codegen() {
+    // Look this variable up in the function.
+    Value *V = NamedValues[Name];
+    if (!V) LogErrorV("Unknown variable name");
+    return V;
+}
+
+// binary expression
+Value *BinaryExprAST::codegen() {
+    Value *L = LHS->codegen();
+    Value *R = RHS->codegen();
+    if (!L || !R) return nullptr;
+    switch (Op) {
+        case '+':
+            return Builder->CreateFAdd(L, R, "addtmp");
+        case '-':
+            return Builder->CreateFSub(L, R, "subtmp");
+        case '*':
+            return Builder->CreateFMul(L, R, "multmp");
+        case '<':
+            L = Builder->CreateFCmpULT(L, R, "cmptmp");
+            // Convert bool 0/1 to double 0.0 or 1.0
+            return Builder->CreateUIToFP(L, Type::getDoubleTy(*TheContext), "booltmp");
+        default:
+            return LogErrorV("invalid binary operator");
+    }
+}
+
+// call
+Value *CallExprAST::codegen() {
+    // Look up the name in the global module table.
+    Function *CalleeF = TheModule->getFunction(Callee);
+    if (!CalleeF) return LogErrorV("Unknown function referenced");
+    // If argument mismatch error.
+    if (CalleeF->arg_size() != Args.size())
+        return LogErrorV("Incorrect # arguments passed");
+
+    std::vector<Value *> ArgsV;
+    for (unsigned i = 0, e = Args.size(); i != e; ++i) {
+        ArgsV.push_back(Args[i]->codegen());
+        if (!ArgsV.back()) return nullptr;
+    }
+    return Builder->CreateCall(CalleeF, ArgsV, "calltmp");
+}
+
+// Function prototype
+Function *PrototypeAST::codegen() {
+    // Make the function type:  double(double,double) etc.
+    // creates a vector of “N” LLVM double types.
+    std::vector<Type*> Doubles(Args.size(), Type::getDoubleTy(*TheContext));
+    // create a function type that takes “N” doubles as arguments, 
+    // returns one double as a result, and that is not vararg
+    FunctionType *FT = FunctionType::get(Type::getDoubleTy(*TheContext), Doubles, false);
+    // creates the IR Function corresponding to the Prototyp
+    Function *F = Function::Create(FT, Function::ExternalLinkage, Name, TheModule.get());
+    // Set names for all arguments.
+    unsigned Idx = 0;
+    for (auto &Arg : F->args())
+        Arg.setName(Args[Idx++]);
+    return F;
+}
+
+// Function body
+Function *FunctionAST::codegen() {
+    // First, check for an existing function from a previous 'extern' declaration.
+    Function *TheFunction = TheModule->getFunction(Proto->getName());
+    if (!TheFunction) TheFunction = Proto->codegen();
+    if (!TheFunction) return nullptr;
+    if (!TheFunction->empty()) return (Function*)LogErrorV("Function cannot be redefined.");
+    // Create a new basic block to start insertion into.
+    BasicBlock *BB = BasicBlock::Create(*TheContext, "entry", TheFunction);
+    Builder->SetInsertPoint(BB);
+
+    // Record the function arguments in the NamedValues map.
+    NamedValues.clear();
+    for (auto &Arg : TheFunction->args())
+        NamedValues[std::string(Arg.getName())] = &Arg;
+    // we call the codegen() method for the root expression of the function.
+    if (Value *RetVal = Body->codegen()) {
+        // Finish off the function.
+        Builder->CreateRet(RetVal);
+        // Validate the generated code, checking for consistency.
+        verifyFunction(*TheFunction);
+        return TheFunction;
+    }
+    // Error reading body, remove function.
+    TheFunction->eraseFromParent();
+    return nullptr;
+}
 //===----------------------------------------------------------------------===//
+// Top-Level parsing and JIT Driver
+//===----------------------------------------------------------------------===//
+
+static void InitializeModule() {
+  // Open a new context and module.
+  TheContext = std::make_unique<LLVMContext>();
+  TheModule = std::make_unique<Module>("my cool jit", *TheContext);
+
+  // Create a new builder for the module.
+  Builder = std::make_unique<IRBuilder<>>(*TheContext);
+}
+
 static void HandleDefinition() {
-  if (ParseDefinition()) {
-    fprintf(stderr, "Parsed a function definition.\n");
+  if (auto FnAST = ParseDefinition()) {
+    if (auto *FnIR = FnAST->codegen()) {
+      fprintf(stderr, "Read function definition:");
+      FnIR->print(errs());
+      fprintf(stderr, "\n");
+    }
   } else {
     // Skip token for error recovery.
     getNextToken();
@@ -364,8 +493,12 @@ static void HandleDefinition() {
 }
 
 static void HandleExtern() {
-  if (ParseExtern()) {
-    fprintf(stderr, "Parsed an extern\n");
+  if (auto ProtoAST = ParseExtern()) {
+    if (auto *FnIR = ProtoAST->codegen()) {
+      fprintf(stderr, "Read extern: ");
+      FnIR->print(errs());
+      fprintf(stderr, "\n");
+    }
   } else {
     // Skip token for error recovery.
     getNextToken();
@@ -374,8 +507,15 @@ static void HandleExtern() {
 
 static void HandleTopLevelExpression() {
   // Evaluate a top-level expression into an anonymous function.
-  if (ParseTopLevelExpr()) {
-    fprintf(stderr, "Parsed a top-level expr\n");
+  if (auto FnAST = ParseTopLevelExpr()) {
+    if (auto *FnIR = FnAST->codegen()) {
+      fprintf(stderr, "Read top-level expression:");
+      FnIR->print(errs());
+      fprintf(stderr, "\n");
+
+      // Remove the anonymous expression.
+      FnIR->eraseFromParent();
+    }
   } else {
     // Skip token for error recovery.
     getNextToken();
@@ -384,29 +524,44 @@ static void HandleTopLevelExpression() {
 
 /// top ::= definition | external | expression | ';'
 static void MainLoop() {
-    while (1) {
-        fprintf(stderr, "ready> ");
-        switch (CurTok) {
-            case tok_eof:    return;
-            case ';':        getNextToken(); break;  // ignore top-level semicolons.
-            case tok_def:    HandleDefinition(); break;
-            case tok_extern: HandleExtern(); break;
-            default:         HandleTopLevelExpression(); break;
-        }
+  while (true) {
+    fprintf(stderr, "ready> ");
+    switch (CurTok) {
+    case tok_eof:
+      return;
+    case ';': // ignore top-level semicolons.
+      getNextToken();
+      break;
+    case tok_def:
+      HandleDefinition();
+      break;
+    case tok_extern:
+      HandleExtern();
+      break;
+    default:
+      HandleTopLevelExpression();
+      break;
     }
+  }
 }
 
-int main () {
-    // while (true) {
-    //     int tok = gettok();
-    //     std::cout<< "go token: " << tok << std::endl;
-    // }
-    // Prime the first token.
-    fprintf(stderr, "ready> ");
-    getNextToken();
+//===----------------------------------------------------------------------===//
+// Main driver code.
+//===----------------------------------------------------------------------===//
 
-    // Run the main "interpreter loop" now.
-    MainLoop();
+int main() {
+  // Prime the first token.
+  fprintf(stderr, "ready> ");
+  getNextToken();
 
-    return 0;
+  // Make the module, which holds all the code.
+  InitializeModule();
+
+  // Run the main "interpreter loop" now.
+  MainLoop();
+
+  // Print out all of the generated code.
+  TheModule->print(errs(), nullptr);
+
+  return 0;
 }
